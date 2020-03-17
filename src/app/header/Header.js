48ac34647f5157fc985c925b504792871a1e5c65
walidMahjoub <walid.kouissah.mahjoub@gmail.com>
2020-03-17 21:58:38 +0100

    link to home on title click

--- src/app/header/Header.js
+++ src/app/header/Header.js
@@ -2,12 +2,15 @@ import React from 'react'
 import style from './Header.scss'
 import {FormattedMessage} from 'react-intl'
 import Language from './Language'
+import {Link} from 'react-router-dom'
 
 const Header = () => (
     <div className={style.header}>
-        <div className={style.title}>
-            <FormattedMessage id="app.header.title"/>
-        </div>
+        <Link to='/'>
+            <div className={style.title}>
+                <FormattedMessage id="app.header.title"/>
+            </div>
+        </Link>
         <Language/>
     </div>
 )
